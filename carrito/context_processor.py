a7571559a5a24333396935903739f8c3e3d50999
astrid0121 <aitageo1980@hotmail.com>
2023-01-24 08:32:11 -0500

    corrigiendo bugs context_processor

--- carrito/context_processor.py
+++ carrito/context_processor.py
@@ -1,6 +1,6 @@
 def total_carrito(request):
     total=0
-    if request.user.is_authenticated and request.session.__contains__("carrito"): 
+    if request.session.__contains__("carrito"): 
         for key, value in request.session["carrito"].items():
             total=total+float(value["precio"])
         
